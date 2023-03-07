1ffa90d85d793cd45b9f4d5f52efe88b1bb32e4b
zu-siwei <2150568643@qq.com>
2023-03-08 07:48:23 +0800

    指针的练习

---
+++ study/study_3_7/test.c
@@ -0,0 +1,34 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+
+int main()
+{
+	int a[5][5];
+	int(*p)[4];
+	p = a;
+	printf("%p,%d\n", &p[4][2] - &a[4][2], &p[4][2] - &a[4][2]);
+	return 0;
+}
+
+#include <stdio.h>
+
+int main()
+{
+	char* a[] = { "work","at","alibaba" };
+	char** pa = a;
+	pa++;
+	printf("%s\n", *pa);
+	return 0;
+}
+
+int main()
+{
+	char* c[] = { "ENTER","NEW","POINT","FIRST" };
+	char** cp[] = { c + 3,c + 2,c + 1,c };
+	char*** cpp = cp;
+	printf("%s\n", **++cpp);//POINT
+	printf("%s\n", *-- * ++cpp + 3);//ER
+	printf("%s\n", *cpp[-2] + 3);//ST
+	printf("%s\n", cpp[-1][-1] + 1);//EW
+	return 0;
+}
