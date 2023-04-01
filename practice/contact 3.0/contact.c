94c2670ca61d0a6b0ccb781adfb84b4c2d0164fd
zu-siwei <2150568643@qq.com>
2023-04-01 22:51:51 +0800

    通讯录文件版

---
+++ practice/contact 3.0/contact.c
@@ -0,0 +1,274 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include "contact.h"
+
+int contact_dila(Contact* contact);//扩容函数
+
+void data_input(Contact* contact)//将文件中的数据导入数组中
+{
+	FILE* pf = fopen("test.data", "rb");
+	if (pf == NULL)
+	{
+		perror("data_input:fopen");
+		return;
+	}
+	Peoinfor tmp = { 0 };
+	while ((fread(&tmp, sizeof(Peoinfor), 1, pf)) == 1)
+	{
+		if(contact->sz == contact->capacity)
+		{
+			if (contact_dila(contact) == 0)
+			{
+				printf("扩容失败\n");
+				return;
+			}
+			else
+			{
+				printf("扩容成功，当前容量：%d\n", contact->capacity);
+			}
+		}
+		contact->num[contact->sz] = tmp;
+		contact->sz++;
+	}
+	fclose(pf);
+	pf = NULL;
+}
+
+
+void contact_init(Contact* contact)
+{
+	contact->num = (Peoinfor*)malloc(sizeof(Peoinfor) * MAX_INI);
+	if (contact->num == NULL)
+	{
+		perror(contact_init);
+		return;
+	}
+	memset(contact->num, 0, sizeof(Peoinfor) * MAX_INI);
+	contact->capacity = MAX_INI;
+	contact->sz = 0;
+
+	//将文件中数据导入数组中，
+	data_input(contact);
+}
+
+
+//扩容通讯录
+int contact_dila(Contact* contact)
+{
+	Peoinfor* ptr = (Peoinfor*)realloc(contact->num, sizeof(Peoinfor) * (contact->capacity + MAX_AD));
+	if (ptr == NULL)
+	{
+		return 0;
+	}
+	else
+	{
+		contact->num = ptr;
+		contact->capacity += MAX_AD;
+		return 1;
+	}
+}
+
+
+void contact_add(Contact* contact)
+{
+	if (contact->sz == contact->capacity)
+	{
+		//扩容
+		if (contact_dila(contact) == 0)
+		{
+			printf("扩容失败\n");
+			return;
+		}
+		else
+		{
+			printf("扩容成功，当前容量：%d\n", contact->capacity);
+		}
+	}
+		printf("请输入姓名:>");
+		scanf("%s", (contact->num)[contact->sz].name);
+		printf("请输入性别:>");
+		scanf("%s", (contact->num)[contact->sz].sex);
+		printf("请输入年龄:>");
+		scanf("%d", &(contact->num)[contact->sz].age);
+		printf("请输入电话:>");
+		scanf("%s", (contact->num)[contact->sz].tele);
+		printf("请输入住址:>");
+		scanf("%s", (contact->num)[contact->sz].address);
+		printf("添加成功\n");
+		contact->sz++;
+}
+
+void contact_del(Contact* contact)
+{
+	if(contact->sz > 0)
+	{
+		char name[MAX_NAME] = {0};
+		printf("请输入被删除联系人姓名:>");
+		scanf("%s", name);
+		int  i = 0;
+		int flag = 0;
+		while (i < contact->sz)
+		{
+			if (strcmp((contact->num)[i].name, name) == 0)
+			{
+				for (i; i < contact->sz - 1; i++)
+				{
+					contact->num[i] = contact->num[i + 1];
+				}
+				flag = 1;
+				printf("删除成功\n");
+				contact->sz--;
+			}
+			i++;
+		}
+		if (flag == 0)
+		{
+			printf("通讯录中没有对应联系人\n");
+		}
+	}
+	else
+	{
+		printf("通讯录中无联系人\n");
+	}
+}
+
+void contact_print(Peoinfor* peo)
+{
+	printf("%-10s %-5s %-4d %-12s %-20s\n", peo->name, peo->sex, peo->age, peo->tele, peo->address);
+}
+
+
+void contact_search(Contact* contact)
+{
+	if (contact->sz > 0)
+	{
+		char name[MAX_NAME] = { 0 };
+		printf("请输入被查找联系人姓名:>");
+		scanf("%s", name);
+		int  i = 0;
+		int flag = 0;
+		while (i < contact->sz)
+		{
+			if (strcmp((contact->num)[i].name, name) == 0)
+			{
+				printf("%-10s %-5s %-3s %-12s %-20s\n", "姓名", "性别", "年龄", "电话", "地址");
+				contact_print(&((contact->num)[i]));
+				flag = 1;
+			}
+			i++;
+		}
+		if (flag == 0)
+		{
+			printf("通讯录中没有对应联系人\n");
+		}
+	}
+	else
+	{
+		printf("通讯录中无联系人，无法查找\n");
+	}
+}
+
+void contact_modify(Contact* contact)
+{
+	if (contact->sz > 0)
+	{
+		char name[MAX_NAME] = { 0 };
+		printf("请输入被查找联系人姓名:>");
+		scanf("%s", name);
+		int  i = 0;
+		int flag = 0;
+		while (i < contact->sz)
+		{
+			if (strcmp((contact->num)[i].name, name) == 0)
+			{
+				printf("请输入姓名:>");
+				scanf("%s", (contact->num)[i].name);
+				printf("请输入性别:>");
+				scanf("%s", (contact->num)[i].sex);
+				printf("请输入年龄:>");
+				scanf("%d", &(contact->num)[i].age);
+				printf("请输入电话:>");
+				scanf("%s", (contact->num)[i].tele);
+				printf("请输入住址:>");
+				scanf("%s", (contact->num)[i].address);
+				printf("修改成功\n");
+				flag = 1;
+			}
+			i++;
+		}
+		if (flag == 0)
+		{
+			printf("通讯录中没有对应联系人,无法修改\n");
+		}
+	}
+	else
+	{
+		printf("通讯录中无联系人，无法修改\n");
+	}
+}
+
+void contact_show(Contact* contact)
+{
+	if(contact->sz>0)
+	{
+		printf("%-10s %-5s %-3s %-12s %-20s\n", "姓名", "性别", "年龄", "电话", "地址");
+		int i = 0;
+		for (i = 0; i < contact->sz; i++)
+		{
+			contact_print(&((contact->num)[i]));
+		}
+	}
+	else
+	{
+		printf("通讯录中无联系人\n");
+	}
+
+}
+
+void contact_clear(Contact* contact)
+{
+	memset(contact->num, 0, sizeof(Peoinfor) * contact->sz);
+	contact->sz = 0;
+	printf("清除成功\n");
+}
+
+int cmp_by_name(const void* e1, const void* e2)
+{
+	return strcmp(((Peoinfor*)(e1))->name, ((Peoinfor*)(e2))->name);
+}
+
+void contact_sort(Contact* contact)
+{
+	if(contact->sz > 0)
+	{
+		qsort(contact->num, contact->sz, sizeof(contact->num[0]), cmp_by_name);
+		printf("排序成功\n");
+	}
+	else
+	{
+		printf("通讯录中无联系人，无法排序\n");
+	}
+}
+
+
+//将数据保存到文件中
+void contact_memory(Contact* contact)
+{
+	FILE* pf = fopen("test.data", "wb");
+	if (pf == NULL)
+	{
+		perror("memory:fopen");
+		return;
+	}
+	int i = 0;
+	for (i = 0; i < contact->sz; i++)
+	{
+		fwrite(contact->num + i, sizeof(Peoinfor), 1, pf);
+	}
+	fclose(pf);
+	pf = NULL;
+}
+
+
+
+
+
---
+++ practice/contact 3.0/test.c
@@ -0,0 +1,87 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include "contact.h"
+//实现一个通讯录；
+//
+//通讯录可以用来存储1000个人的信息，每个人的信息包括：姓名、性别、年龄、电话、住址
+//
+//提供方法：
+//
+//添加联系人信息
+//删除指定联系人信息
+//查找指定联系人信息
+//修改指定联系人信息
+//显示所有联系人信息
+//清空所有联系人
+//以名字排序所有联系人
+
+
+void menu()
+{
+	printf("************************************\n");
+	printf("****    1. add      2. del      ****\n");
+	printf("****    3. search   4. modify   ****\n");
+	printf("****    5. show     6. all_clear****\n");
+	printf("****    7. sort     0. exit     ****\n");
+	printf("************************************\n");
+}
+
+enum S
+{
+	EXIT,
+	ADD,
+	DEL,
+	SEARCH,
+	MODIFY,
+	SHOW,
+	ALL_CLEAR,
+	SORT
+};
+
+int main()
+{
+	Contact contact = {0};
+	contact_init(&contact);
+	int input = 0;
+	do
+	{
+		menu();
+		printf("请选择:>");
+		scanf("%d", &input);
+		switch (input)
+		{
+		case ADD:
+			contact_add(&contact);
+			break;
+		case DEL:
+			contact_del(&contact);
+			break;
+		case SEARCH:
+			contact_search(&contact);
+			break;
+		case MODIFY:
+			contact_modify(&contact);
+			break;
+		case SHOW:
+			contact_show(&contact);
+			break;
+		case ALL_CLEAR:
+			contact_clear(&contact);
+			break;
+		case SORT:
+			contact_sort(&contact);
+			break;
+		case EXIT:
+			contact_memory(&contact);
+			free(contact.num);
+			contact.num = NULL;
+			printf("数据保存成功\n");
+			printf("通讯录销毁成功\n");
+			printf("退出通讯录\n");
+			break;
+		default:
+			printf("输入错误，请重新输入\n");
+			break;
+		}
+	}while (input);
+	return 0;
+}
