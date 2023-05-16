4c880bcc68156b557965e446b97e1e55836f7e19
zu-siwei <2150568643@qq.com>
2023-05-16 22:43:43 +0800

    栈

--- practice/Stack/Stack.c
+++ practice/Stack/Stack.c
@@ -1,1 +1,71 @@
 #define _CRT_SECURE_NO_WARNINGS 1
+#include "Stack.h"
+
+void StackInit(Stack* ps)
+{
+	ps->a = NULL;
+	ps->capacity = 0;
+	ps->top = 0;
+}
+
+void StackPush(Stack* ps, STDataType data)
+{
+	assert(ps);
+
+	if (ps->capacity == ps->top)
+	{
+		int newcapacity = ps->capacity == 0 ? 4 : ps->capacity * 2;
+		STDataType* tmp = (STDataType*)realloc(ps->a, newcapacity * sizeof(STDataType));
+		if (tmp == NULL)
+		{
+			perror("realloc fail");
+			return;
+		}
+		ps->a = tmp;
+		ps->capacity = newcapacity;
+	}
+	ps->a[ps->top] = data;
+	ps->top++;
+}
+
+void StackPop(Stack* ps)
+{
+	assert(ps);
+	assert(ps->top > 0);
+
+	ps->top--;
+}
+
+STDataType StackTop(Stack* ps)
+{
+	assert(ps);
+	assert(ps->top > 0);
+
+	return ps->a[ps->top - 1];
+}
+
+int StackSize(Stack* ps)
+{
+	assert(ps);
+
+	return ps->top;
+}
+
+int StackEmpty(Stack* ps)
+{
+	assert(ps);
+
+	return ps->top == 0;
+}
+
+void StackDestroy(Stack* ps)
+{
+	free(ps->a);
+	ps->a = NULL;
+	ps->capacity = ps->top = 0;
+}
+
+
+
+
+
--- practice/Stack/test.c
+++ practice/Stack/test.c
@@ -1,1 +1,32 @@
 #define _CRT_SECURE_NO_WARNINGS 1
+#include "Stack.h"
+
+void TestStack()
+{
+	Stack st;
+	StackInit(&st);
+
+	StackPush(&st, 1);
+	StackPush(&st, 2);
+	StackPush(&st, 3);
+	printf("%d ", StackTop(&st));
+	StackPop(&st);
+	StackPush(&st, 4);
+	StackPush(&st, 5);
+	StackPush(&st, 6);
+
+	while (StackSize(&st))
+	{
+		printf("%d ", StackTop(&st));
+		StackPop(&st);
+	}
+
+	StackDestroy(&st);
+}
+
+
+int main()
+{
+	TestStack();
+	return 0;
+}
