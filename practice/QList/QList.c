2c74f8c0f0e3c4c652c509d055c406be32e6802e
zu-siwei <2150568643@qq.com>
2023-05-17 23:24:19 +0800

    列表

--- practice/QList/QList.c
+++ practice/QList/QList.c
@@ -1,1 +1,108 @@
 #define _CRT_SECURE_NO_WARNINGS 1
+#include "QList.h"
+
+void QueueInit(Queue* q)
+{
+	assert(q);
+
+	q->_front = NULL;
+	q->_rear = NULL;
+}
+
+void QueuePush(Queue* q, QDataType data)
+{
+	assert(q);
+
+	QNode* NewData = (QNode*)malloc(sizeof(QNode));
+	if (NewData == NULL)
+	{
+		perror("malloc fail");
+		return;
+	}
+	NewData->data = data;
+	NewData->next = NULL;
+	if (q->_front == NULL)
+	{
+		q->_front = NewData;
+		q->_rear = NewData;
+	}
+	else
+	{
+		q->_rear->next = NewData;
+		q->_rear = NewData;
+	}
+}
+
+void QueuePop(Queue* q)
+{
+	assert(q);
+	assert(!QueueEmpty(q));
+
+	if (q->_front->next == NULL)
+	{
+		free(q->_front);
+		q->_front = NULL;
+		q->_rear = NULL;
+	}
+	else
+	{
+		QNode* cur = q->_front;
+		q->_front = q->_front->next;
+		free(cur);
+	}
+}
+
+QDataType QueueFront(Queue* q)
+{
+	assert(q);
+	assert(!QueueEmpty(q));
+
+	return q->_front->data;
+}
+
+QDataType QueueBack(Queue* q)
+{
+	assert(q);
+	assert(!QueueEmpty(q));
+
+	return q->_rear->data;
+}
+
+int QueueSize(Queue* q)
+{
+	assert(q);
+
+	QNode* cur = q->_front;
+	int count = 0;
+	while (cur)
+	{
+		count++;
+		cur = cur->next;
+	}
+	return count;
+}
+
+int QueueEmpty(Queue* q)
+{
+	assert(q);
+
+	return q->_front == NULL;
+}
+
+void QueueDestroy(Queue* q)
+{
+	assert(q);
+
+	QNode* cur = q->_front;
+	while (cur != NULL)
+	{
+		QNode* next = cur->next;
+		free(cur);
+		cur = next;
+	}
+	q->_front = NULL;
+	q->_rear = NULL;
+}
+
+
+
--- practice/QList/test.c
+++ practice/QList/test.c
@@ -1,1 +1,29 @@
 #define _CRT_SECURE_NO_WARNINGS 1
+#include "QList.h"
+
+void TestQList()
+{
+	Queue q;
+	QueueInit(&q);
+
+	QueuePush(&q, 1);
+	QueuePush(&q, 2);
+	QueuePush(&q, 3);
+	QueuePush(&q, 4);
+	printf("%d\n",QueueSize(&q));
+	QueuePush(&q, 5);
+	QueuePush(&q, 6);
+
+	while (!QueueEmpty(&q))
+	{
+		printf("%d ", QueueFront(&q));
+		QueuePop(&q);
+	}
+	QueueDestroy(&q);
+}
+
+int main()
+{
+	TestQList();
+	return 0;
+}
