acbeb3af8049e6e833afd40418c78ee696d2807a
zu-siwei <2150568643@qq.com>
2023-05-23 22:26:39 +0800

    二叉树的基础操作

---
+++ practice/practice_5_23/test.c
@@ -0,0 +1,141 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+
+
+typedef int BTDataType;
+typedef struct BinaryTreeNode
+{
+	BTDataType data;
+	struct BinaryTreeNode* left;
+	struct BinaryTreeNode* right;
+}BTNode;
+
+BTNode* BuyNode(int x)
+{
+	BTNode* TreeNode = (BTNode*)malloc(sizeof(BTNode));
+	if (TreeNode == NULL)
+	{
+		perror("malloc fail");
+		return NULL;
+	}
+	TreeNode->data = x;
+	TreeNode->left = NULL;
+	TreeNode->right = NULL;
+}
+
+BTNode* CreatBinaryTree()
+{
+	BTNode* node1 = BuyNode(1);
+	BTNode* node2 = BuyNode(2);
+	BTNode* node3 = BuyNode(3);
+	BTNode * node4 = BuyNode(4);
+	BTNode* node5 = BuyNode(5);
+	BTNode* node6 = BuyNode(6);
+	BTNode* node7 = BuyNode(7);
+	node1->left = node2;
+	node1->right = node4;
+	node2->left = node3;
+	node4->left = node5;
+	node4->right = node6;
+	node5->left = node7;
+	return node1;
+}
+
+// 二叉树前序遍历
+void PreOrder(BTNode* root)
+{
+	if (root == NULL)
+	{
+		printf("N ");
+		return;
+	}
+	printf("%d ", root->data);
+	PreOrder(root->left);
+	PreOrder(root->right);
+}
+// 二叉树中序遍历
+void InOrder(BTNode* root)
+{
+	if (root == NULL)
+	{
+		printf("N ");
+		return;
+	}
+	InOrder(root->left);
+	printf("%d ", root->data);
+	InOrder(root->right);
+}
+// 二叉树后序遍历
+void PostOrder(BTNode* root)
+{
+	if (root == NULL)
+	{
+		printf("N ");
+		return;
+	}
+	PostOrder(root->left);
+	PostOrder(root->right);
+	printf("%d ", root->data);
+}
+
+// 二叉树节点个数
+int BinaryTreeSize(BTNode* root)
+{
+	if (root == NULL)
+	{
+		return 0;
+	}
+	return BinaryTreeSize(root->left)
+		 + BinaryTreeSize(root->right)
+		 + 1;
+}
+// 二叉树叶子节点个数
+int BinaryTreeLeafSize(BTNode* root)
+{
+	if (root == NULL)
+	{
+		return 0;
+	}
+
+	if (root->left == NULL && root->right == NULL)
+	{
+		return 1;
+	}
+
+	return BinaryTreeLeafSize(root->left) + BinaryTreeLeafSize(root->right);
+}
+// 二叉树第k层节点个数
+int BinaryTreeLevelKSize(BTNode* root, int k)
+{
+	if (root == NULL)
+	{
+		return 0;
+	}
+
+	if (k == 1)
+	{
+		return 1;
+	}
+
+	return BinaryTreeLevelKSize(root->left, k - 1) + BinaryTreeLevelKSize(root->right, k - 1);
+}
+
+int main()
+{
+	BTNode* root = CreatBinaryTree();
+	PreOrder(root);
+	printf("\n");
+	InOrder(root);
+	printf("\n");
+	PostOrder(root);
+	printf("\n");
+
+	printf("BinaryTreeSize:%d\n", BinaryTreeSize(root));
+	printf("BinaryTreeLeafSize:%d\n", BinaryTreeLeafSize(root));
+	printf("BinaryTreeLevelKSize:%d\n", BinaryTreeLevelKSize(root, 3));
+	printf("BinaryTreeLevelKSize:%d\n", BinaryTreeLevelKSize(root, 4));
+
+	return 0;
+}
