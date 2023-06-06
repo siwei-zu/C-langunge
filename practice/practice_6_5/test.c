c43601db940db6702bb3241285b0b3414c89071d
zu-siwei <2150568643@qq.com>
2023-06-06 22:01:11 +0800

    今日练习

--- practice/practice_6_5/test.c
+++ practice/practice_6_5/test.c
@@ -1,1 +1,18 @@
 #define _CRT_SECURE_NO_WARNINGS 1
+struct ListNode* deleteMiddle(struct ListNode* head) {
+    if (head->next == NULL)
+    {
+        return NULL;
+    }
+
+    struct ListNode* slow = head;
+    struct ListNode* fast = head;
+    struct ListNode* pre = NULL;
+    while (fast && fast->next) {
+        fast = fast->next->next;
+        pre = slow;
+        slow = slow->next;
+    }
+    pre->next = pre->next->next;
+    return head;
+}
